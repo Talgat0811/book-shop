905728476b3d9077a1adfdde443b3a953adb7ef7
talgat <talgat.birimkulov@gmail.com>
2023-09-15 15:08:30 +0600

    first title

--- src/main/java/com/example/book/MainController.java
+++ src/main/java/com/example/book/MainController.java
@@ -1,2 +1,14 @@
-package com.example.book;public class MainController {
+package com.example.book;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mars")
+public class MainController {
+    @GetMapping("/admin")
+    public String mainPage(){
+        return "index";
+    }
 }
