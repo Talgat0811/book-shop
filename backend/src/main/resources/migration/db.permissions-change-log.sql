f9feda25b112b7e0111849454cc010a33c999a85
YoungG-rgb <zhtoichubaev@mail.ru>
2023-09-28 00:07:25 +0600

    добавление общих пермишонов

--- backend/src/main/resources/migration/db.permissions-change-log.sql
+++ backend/src/main/resources/migration/db.permissions-change-log.sql
@@ -19,3 +19,28 @@ values (
 
 -- У пользователя нет доступов к удалению или добавлению книг (а чтение книг ОТКРЫТО для всех)
 -------------------------------------------------------------------------------------------------------
+-- Добавление общих пермишонов
+insert into auth_permissions(id, description, name)
+values (nextval('auth_permissions_seq'), 'Чтение данных', 'READ');
+insert into auth_permissions(id, description, name)
+values (nextval('auth_permissions_seq'), 'Удаление данных', 'DELETE');
+insert into auth_permissions(id, description, name)
+values (nextval('auth_permissions_seq'), 'Запись данных', 'WRITE');
+
+-- Настройка пермишонов
+-- У админа имеется доступ ко всему
+insert into auth_roles_permissions(auth_role_id, auth_permission_id)
+values (
+        (select id from auth_roles where name='ADMIN'),
+        (select id from auth_permissions where name='READ')
+       );
+insert into auth_roles_permissions(auth_role_id, auth_permission_id)
+values (
+           (select id from auth_roles where name='ADMIN'),
+           (select id from auth_permissions where name='DELETE')
+       );
+insert into auth_roles_permissions(auth_role_id, auth_permission_id)
+values (
+           (select id from auth_roles where name='ADMIN'),
+           (select id from auth_permissions where name='WRITE')
+       );
