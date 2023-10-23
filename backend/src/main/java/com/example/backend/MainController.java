bd7b67d64fcfc2795589e77a8e8da57047dae8fb
zhtoichubaev <zhtoichubaev@mail.ru>
2023-10-24 03:40:54 +0600

    add fucking front

--- backend/src/main/java/com/example/backend/MainController.java
+++ backend/src/main/java/com/example/backend/MainController.java
@@ -2,12 +2,10 @@ package com.example.backend;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/mars")
 public class MainController {
-    @GetMapping("/admin")
+    @GetMapping
     public String mainPage(){
         return "index";
     }
